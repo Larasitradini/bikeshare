36be282fd34df91e4340bc786b1e9af2dcbc7d2f
Larasitradini <147293951+Larasitradini@users.noreply.github.com>
2023-11-18 03:22:34 +0700

    Update bike-sharing-dashboard_plotly.py

--- bike-sharing-dashboard_plotly.py
+++ bike-sharing-dashboard_plotly.py
@@ -1,13 +1,8 @@
 
-
 # Import Library
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
-# ==============================
-# LOAD DATA
-# ==============================
 
 
 @st.cache_resource
@@ -19,10 +14,6 @@ def load_data():
 data = load_data()
 
 
-# ==============================
-# TITLE DASHBOARD
-# ==============================
-# Set page title
 st.title("Bike Share Dashboard")
 
 
@@ -46,10 +37,6 @@ st.sidebar.markdown('2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, 
 st.sidebar.markdown('3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds')
 st.sidebar.markdown('4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog')
 
-
-# ==============================
-# VISUALIZATION
-# ==============================
 
 # create a layout with two columns
 col1, col2 = st.columns(2)
@@ -79,16 +66,14 @@ with col2:
     st.plotly_chart(fig_weather_count, use_container_width=True,height=400, width=800)
 
 
-# Hourly bike share count
-# st.subheader("Hourly Bike Share Count")
+
 hourly_count = data.groupby("hr")["cnt"].sum().reset_index()
 fig_hourly_count = px.line(
     hourly_count, x="hr", y="cnt", title="Hourly Bike Share Count")
 st.plotly_chart(fig_hourly_count, use_container_width=True,
                 height=400, width=600)
 
-# Humidity vs. Bike Share Count
-# st.subheader("Humidity vs. Bike Share Count")
+
 fig_humidity_chart = px.scatter(
     data, x="hum", y="cnt", title="Humidity vs. Bike Share Count")
 st.plotly_chart(fig_humidity_chart)
@@ -99,8 +84,7 @@ fig_wind_speed_chart = px.scatter(
     data, x="windspeed", y="cnt", title="Wind Speed vs. Bike Share Count")
 st.plotly_chart(fig_wind_speed_chart)
 
-# Temperature vs. Bike Share Count
-# st.subheader("Temperature vs. Bike Share Count")
+
 fig_temp_chart = px.scatter(data, x="temp", y="cnt",
                             title="Temperature vs. Bike Share Count")
 st.plotly_chart(fig_temp_chart, use_container_width=True,
